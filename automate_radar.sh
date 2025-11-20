4e4c99f51d8fac0e167381e53bafb6b7ac55ce6c
ield <nachol2013@gmail.com>
2025-11-20 12:34:02 +0100

    Fixed automated_radar.sh. Now, it saves the log messages into a file and runs the python script without using nohub. Added a file with the contents of the radar_service_file

--- automate_radar.sh
+++ automate_radar.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+#Generate a log file to print the output
+exec > /home/gmr-lab/Desktop/radar_home/meas/debug.log 2>&1
+set -x
+
 # Go to project directory
 cd /home/gmr-lab/Desktop/radar_home/ || exit 1
 
 # Update git repository
+echo "Pulling git ..."
 git pull
 
 # Activate Python environment
+echo "Activating env..."
 source /home/gmr-lab/Desktop/radar_home/env_home/bin/activate
 
-# Create daily log file
-LOGFILE="/home/gmr-lab/Desktop/radar_home/meas/log_$(date +%Y%m%d).txt"
+#Check the python version
+which python3
+python3 --version
 
+# Create daily log file
+echo "Running Python Script..."
+LOGFILE="/home/gmr-lab/Desktop/radar_home/meas/log_$(date +%Y%m%d).txt"
 # Run Python script in background with unbuffered output
-nohup python3 -u cw_multinode.py > "$LOGFILE" 2>&1 < /dev/null &
+exec python3 cw_multinode.py >> "$LOGFILE" 2>&1
+
+echo "Process completed"
